90af1077d473452ecc324cff06801a2f4bb31eed
khushishah-703 <khushifshah0101@gmail.com>
2024-02-14 12:43:40 +0530

    Initial commit 2

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // App Setup (app.js)
 
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import scheduledTask from "./scheduledTask"
 
 const app = express();
 const port = 8080;
 
 mongoose
   .connect(
-    "mongodb+srv://dbUser:dbUSERPASSWORD@cluster0.qkgmjl5.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://khushifshah0102:cey4PouJ2cgNkgLr@cluster0.tipj1a4.mongodb.net/?retryWrites=true&w=majority"
   )
-  .then(() => app.listen(8080))
-  .then(() => console.log("listening...."));
-
-scheduledTasks.start();
+  .then(() => {
+    app.listen(8080);
+    console.log("listening....");
+    // schedule the task to run every day at 12:00 AM
+    task.scheduleTask();
+  });
+  
 
 app.get("/", (req, res) => {
   res.send("Hi khushi!");
 });
+
+//cey4PouJ2cgNkgLr
+//khushifshah0102
